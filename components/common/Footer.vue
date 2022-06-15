712774a32b5b6e440d670ba30be5807504f51592
Ana Júlia <analinsdev@gmail.com>
2022-06-15 14:55:25 -0300

    teste

---
+++ components/common/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer :absolute="!fixed" app>
+    <span>&copy; {{ new Date().getFullYear() }}</span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      fixed: false,
+    };
+  },
+};
+</script>
---
+++ components/common/Menu.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-spacer />
+    <v-btn icon>
+      <v-icon>mdi-cards </v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-clipboard-text-clock </v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js",
+    };
+  },
+};
+</script>
